ee24389eb0b5a693e632359e76f6a7c08e21e177
Abhay Keshari <instantboostbyabhaykeshari@gmail.com>
2026-06-20 05:38:31 +0530

    Fix deployment error of ui

--- Client/src/config/index.js
+++ Client/src/config/index.js
@@ -1,7 +1,7 @@
 const config = {
   BASE_URL: process.env.NEXT_PUBLIC_API_URL
     || process.env.REACT_APP_API_URL
-    || 'http://localhost:5000',
+    || 'http://localhost:4000',
 };
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
